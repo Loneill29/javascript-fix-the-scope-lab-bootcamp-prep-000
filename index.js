cb4ee654411e3807b05af8120a207dc435db0ba6
Linda ONeill <oneill.linda21@gmail.com>
2017-11-06 23:37:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function add2(n) { const two = 2
 
 
 }
+function funkyFunction() {var theFunk = 'FUNKY!'
+return theFunk}
 
-var theFunk = funkyFunction()
-function funkyFunction() {return "FUNKY!"}
